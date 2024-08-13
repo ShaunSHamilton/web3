d16cc278c73ec3c2fd7d9210fcaffeb676f7ba43
bos-hieu <12520839@gm.uit.edu.vn>
2024-08-13 01:35:27 -0400

    feat: add data to block

--- learn-digital-ledgers-by-building-a-blockchain/add-block.js
+++ learn-digital-ledgers-by-building-a-blockchain/add-block.js
@@ -4,8 +4,12 @@ const blockchain = getBlockchain();
 const previousBlock = blockchain[blockchain.length-1];
 const newBlock = {
     hash: Math.random().toString(),
-    previousHash: previousBlock.hash
+    previousHash: previousBlock.hash,
+    data: {
+        fromAddress: process.argv[2],
+        toAddress: process.argv[3],
+        amount: parseInt(process.argv[4])
+    }
 };
 blockchain.push(newBlock);
 writeBlockchain(blockchain);
-// console.log(blockchain);
