75ab52df6e1f61adc5fb94b6260253c31ad3c43a
bos-hieu <12520839@gm.uit.edu.vn>
2024-08-13 00:59:45 -0400

    update

--- build-a-video-game-marketplace-blockchain/init-blockchain.js
+++ build-a-video-game-marketplace-blockchain/init-blockchain.js
@@ -1,2 +1,9 @@
 import { writeBlockchain, writeTransactions } from './blockchain-helpers.js';
 // Add your code below
+
+writeBlockchain([
+    {
+      "hash": "0",
+      "previousHash": null
+    },
+])
