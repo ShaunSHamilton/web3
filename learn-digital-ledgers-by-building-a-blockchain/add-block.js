9fa9667c7d03868db9ab0af5d1087b0a6a5f02c5
bos-hieu <12520839@gm.uit.edu.vn>
2024-08-13 01:46:56 -0400

    feat: finish lesson

--- learn-digital-ledgers-by-building-a-blockchain/add-block.js
+++ learn-digital-ledgers-by-building-a-blockchain/add-block.js
@@ -1,15 +1,13 @@
-import { getBlockchain, writeBlockchain } from "./blockchain-helpers.js";
+import { getBlockchain, writeBlockchain, getTransactions, writeTransactions } from "./blockchain-helpers.js";
 
 const blockchain = getBlockchain();
 const previousBlock = blockchain[blockchain.length-1];
+const transactions = getTransactions();
 const newBlock = {
     hash: Math.random().toString(),
     previousHash: previousBlock.hash,
-    data: {
-        fromAddress: process.argv[2],
-        toAddress: process.argv[3],
-        amount: parseInt(process.argv[4])
-    }
+    transactions,
 };
 blockchain.push(newBlock);
 writeBlockchain(blockchain);
+writeTransactions([])
